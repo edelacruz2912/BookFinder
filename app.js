d9b357ac56edfc4f8ee59036c2ae85022b3850d1
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2017-08-24 22:36:14 +0000

    intial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+var bodyParser = require("body-parser");
+
+//APP CONFIGURATION
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//ROUTES 
+
+app.get("/",function(req,res)
+{
+    res.render("landing");
+});
+
+
+app.get("/form",function(req, res) {
+    res.render("form");
+})
+
+app.get("/results", function(req, res){
+    var query = req.query.searchs;
+    var url = "http://it-ebooks-api.info/v1/search/" + query;
+    
+    request(url , function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("results", {data: data,input: query});
+            //console.log(data["Books"][0]["Title"]);
+        }
+    });
+});
+
+
+
+
+
+//Server listener 
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("SERVER IS RUNNING!");
+});
+
+
+
+
+
